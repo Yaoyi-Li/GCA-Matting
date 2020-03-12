187b61ebbb24983d58edd8b10e11961981ade81b
Li Yaoyi <dsamuel@sjtu.edu.cn>
2020-03-13 02:23:50 +0800

    Update copy_testing_alpha.sh
    fix a bug 🐛

--- copy_testing_alpha.sh
+++ copy_testing_alpha.sh
@@ -2,7 +2,7 @@
 test_set_path=$1
 mkdir "$1/alpha_copy"
 
-for filename in $1/alpha/*.png; do
+for filename in "$1"/alpha/*.png; do
     echo $filename
     for ((i=0; i<=19; i++)); do
         cp "$filename" "$1/alpha_copy/$(basename "$filename" .png)_$i.png"
